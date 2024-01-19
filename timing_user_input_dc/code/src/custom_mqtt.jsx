ffafed56dec4cc4a0968d0aa7282160ba1ecbaa9
bacon274 <jfm66@cam.ac.uk>
2024-01-19 14:58:28 +0000

    Custom Reducer implemented

--- timing_user_input_dc/code/src/custom_mqtt.jsx
+++ timing_user_input_dc/code/src/custom_mqtt.jsx
@@ -1,11 +1,50 @@
 export async function custom_new_message_action(dispatch, message) {
   console.log(message)
   console.log('*** custom_new_message_action ***')
+  if (message && message.topic.match("location_state/update")) {
+    dispatch({ type: 'ITEM_UPDATE', updated_entry: message.payload })
+  } else if (message && message.topic.match("location_state/entered")) {
+    dispatch({ type: 'ITEM_ENTERED', added_entry: message.payload })
+  } else if (message && message.topic.match("location_state/exited")) {
+    dispatch({ type: 'ITEM_EXITED', removed_entry: message.payload })
+  }
 }
 
+
 export const CustomReducer = (currentState, action) => {
-  // console.log(action)
+  console.log("*** CustomReducer ***")
+  console.log("Action: ", action)
+  console.log("Current State: (1):  ", currentState)
+  let filtered_items_state = []
+  let new_item_history = []
+
   switch (action.type) {
+    case 'ITEM_ENTERED':
+      console.log("Action State: " , action.added_entry.state)
+      console.log("Current State.state " , currentState.items_state)
+      console.log("Current items from", action.added_entry.state, ": ", currentState.items_state[action.added_entry.state] )
+      console.log("Filtered State: " , filtered_items_state)
+      
+      return {
+        ...currentState,
+        items_state: {
+          ...currentState.items_state,
+          [action.added_entry.state]: [...currentState.items_state[action.added_entry.state], action.added_entry]
+        }
+      }
+    case 'ITEM_EXITED':
+      // console.log("Action State: " , action.removed_entry.state)
+      // console.log("Current State: " , currentState.items_state)
+      // console.log("Current items from", action.removed_entry.state, ": ", currentState.items_state[action.removed_entry.state] )
+      filtered_items_state = currentState.items_state[action.removed_entry.state].filter(item => !(item.item_id === action.removed_entry.item_id && item.location_link === action.removed_entry.location_link))
+      // console.log("Filtered State: " , filtered_items_state)
+      return {
+        ...currentState,
+        items_state: {
+          ...currentState.items_state,
+          [action.removed_entry.state]: filtered_items_state
+        }
+      }
     case 'MQTT_STATUS':
       return {
         ...currentState,
