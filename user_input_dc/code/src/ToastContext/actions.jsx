7d73ecf3557f6bbcb7238c0012241f6987b50025
bacon274 <jfm66@cam.ac.uk>
2024-04-09 10:00:56 +0100

    cleanup

--- user_input_dc/code/src/ToastContext/actions.jsx
+++
@@ -1,7 +0,0 @@
-export async function add_toast(dispatch,toast) {
-  dispatch({ type: 'ADD_TOAST', new_toast: toast });
-}
-
-export async function remove_toast(dispatch, key) {
-  dispatch({ type: 'REMOVE_TOAST',remove_key: key})
-}
--- user_input_dc/code/src/ToastContext/index.jsx
+++
@@ -1,4 +0,0 @@
-import { add_toast} from './actions';
-import { ToastProvider, useToastDispatch } from './context';
- 
-export {ToastProvider,useToastDispatch,add_toast};
